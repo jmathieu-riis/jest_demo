b1d04e6493eb13af598edbd11c6528afd9b29284
Jacques Mathieu <jmathieu@riis.com>
2021-06-29 10:25:48 -0400

    Spy on trim func

--- test/util.test.js
+++ test/util.test.js
@@ -2,7 +2,8 @@ import cleanInputString from '../src/util';
 
 describe('test util.js', () => {
     test('should trim input string', () => {
-        let cleanString = cleanInputString(' testInputString ');
-        expect(cleanString).toEqual('testInputString');
+        const trimSpy = jest.spyOn(String.prototype, 'trim');
+        cleanInputString(' testInputString ');
+        expect(trimSpy).toHaveBeenCalled();
     });
 });
